c3439b82fe038f4eb1ea7b312a249d0c5c12bcf8
masterUNG <phrombutr@gmail.com>
2019-11-16 14:24:21 +0700

    Start git

---
+++ lib/screens/home.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // Field
+
+  // Method
+  Widget signInButton() {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      color: Colors.red[900],
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      borderSide: BorderSide(color: Colors.red),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Text(
+        'Sign Up',
+        style: TextStyle(color: Colors.red[900]),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        signInButton(),
+        SizedBox(
+          width: 5.0,
+        ),
+        signUpButton(),
+      ],
+    );
+  }
+
+  Widget userText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.perm_identity,
+            size: 36.0,
+            color: Colors.red[900],
+          ),
+          labelText: 'User :',
+        ),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock,
+            size: 36.0,
+            color: Colors.red[900],
+          ),
+          labelText: 'Password :',
+        ),
+      ),
+    );
+  }
+
+  Widget showLogo() {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'Ung Pop',
+      style: TextStyle(
+        color: Colors.red[900],
+        fontSize: 30.0,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        fontFamily: 'PermanentMarker',
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/wall.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(180, 255, 255, 255),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              padding: EdgeInsets.all(18.0),
+              // color: Color.fromARGB(180, 255, 255, 255),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    showLogo(),
+                    showAppName(),
+                    userText(),
+                    passwordText(),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    showButton(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
